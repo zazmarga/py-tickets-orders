0ca9096288b1e1f868aa3ee65ba1092248266f93
Marharyta Zazulia <zazmargo@gmail.com>
2024-12-10 23:26:47 -0300

    fix bugs flake8 in settings.py

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -138,6 +138,7 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 1
+    "DEFAULT_PAGINATION_CLASS":
+        "rest_framework.pagination.LimitOffsetPagination",
+    "PAGE_SIZE": 1
 }
